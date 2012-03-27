5a7a81e7294ca579155836bb69f7662596ea119f
windwild <gjywindwild@gmail.com>
2012-03-27 16:16:41 +0800

    fixed a bug an extra point.....

--- renderer.php
+++ renderer.php
@@ -11,7 +11,27 @@ class local_courseranker_renderer extends plugin_renderer_base{
 	
 	function coursetable(){
 		$output = '';
-		$output .= get_course_table();
+		$results = get_course_table();
+		$output .= '<table border=1>';
+		if(count($results) > 0){
+			$output .= '<tr>';
+			foreach ($results[0] as $key => $value){
+				$output .= '<td>'.$key.'</td>';
+			}
+			$output .= '</tr>';
+		}
+		//print result table
+		foreach ($results as $result){
+			$output .= '<tr>'.
+				'<td>'.'<a href="?course_id='.$result['id'].'">'.$result['id'].'</a></td>'.
+				'<td>'.$result['fullname'].'</td>'.
+				'<td>'.$result['view_num'].'</td>'.
+				'<td>'.$result['post_num'].'</td>'.
+				'<td>'.$result['score'].'</td>'.
+				'<tr>';
+		}
+		$output .= '</table>';
+		
 		return $output;
 	}
 	
