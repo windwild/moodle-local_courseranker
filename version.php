fe9f4d7dc6fb9e67b1d4ca7a2415c1c318efb10b
windwild <gjywindwild@gmail.com>
2012-04-01 09:14:50 +0800

    add comment info to lib.php and version.php

--- version.php
+++ version.php
@@ -1,8 +1,15 @@
 <?php
+
+/////////////////////////////////////////////////////////////////////////////////
+///  Code fragment to define the version of Course Ranker
+///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
+/////////////////////////////////////////////////////////////////////////////////
+
+
 $plugin->version  = 2012040100;   // The (date) version of this plugin
 $plugin->requires = 2010122900;   // Requires this Moodle version
 
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = "1.0";       // User-friendly version number
 
-$plugin->cron = 10*60;
+$plugin->cron = 60*60;
